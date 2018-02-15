2b3f06e81f474865ec83d9757dcba211f4c45c9f
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-15 12:09:42 +0100

    Models ok

--- db/migrate/20180215105814_create_doses.rb
+++ db/migrate/20180215105814_create_doses.rb
@@ -3,7 +3,7 @@ class CreateDoses < ActiveRecord::Migration[5.1]
     create_table :doses do |t|
       t.string :description
       t.references :cocktail, foreign_key: true
-      t.refrences :ingredient
+      t.references :ingredient
 
       t.timestamps
     end
