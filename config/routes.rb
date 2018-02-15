53d0d36ed607f340f2723b82f453b2b0b79998d6
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-02-15 14:39:57 +0100

    CocktailsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  get 'doses/new'
+
+  get 'doses/destroy'
+
+  get 'doses/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [ :index, :new, :show, :create ]
 end
+
+
+# resources :restaurants do
+#     resources :reviews, only: [ :index, :new, :create ]
+#   end
+#   resources :reviews, only: [ :show, :edit, :update, :destroy ]
+# end
